22c42e8800d72057a110223246306b0995cc1965
franciscocostalap <franciscocosta.py@gmail.com>
2020-11-25 14:27:20 +0000

    Version 1 Racket drawn.

---
+++ src/main/kotlin/Racket.kt
@@ -0,0 +1,52 @@
+import pt.isel.canvas.Canvas
+import pt.isel.canvas.WHITE
+
+data class Racket(val x:Int, val y:Int)
+
+/**
+ * Main rectangle width.
+ */
+const val RACKET_WIDTH = 90
+/**
+ * Main rectangle height.
+ *
+ * Must be even.
+ */
+const val RACKET_HEIGHT = 10
+
+/**
+ * Corner rectangle width and color.
+ */
+const val CORNER_WIDTH = 10
+const val CORNER_COLOR = 0xFF3333
+
+/**
+ * Intermediate rectangle width and color.
+ */
+const val MEDIUM_WIDTH = 15
+const val MEDIUM_COLOR = 0xFF6666
+
+/**
+ * Racket starting x.
+ */
+const val RACKET_X = 155
+/**
+ * Racket y.
+ */
+const val RACKET_Y = 550
+
+fun Canvas.drawRacket(rket:Racket) {
+    fun drawCorners(rket: Racket) {
+        drawRect(rket.x , RACKET_Y, CORNER_WIDTH, RACKET_HEIGHT / 2, CORNER_COLOR)
+        drawRect(rket.x + (RACKET_WIDTH - CORNER_WIDTH), RACKET_Y, CORNER_WIDTH, RACKET_HEIGHT / 2, CORNER_COLOR)
+    }
+    fun drawEdges(rket: Racket){
+        drawRect(rket.x + CORNER_WIDTH, rket.y, MEDIUM_WIDTH, RACKET_HEIGHT / 2, MEDIUM_COLOR)
+        drawRect(rket.x + RACKET_WIDTH - CORNER_WIDTH - MEDIUM_WIDTH, rket.y, MEDIUM_WIDTH, RACKET_HEIGHT / 2, MEDIUM_COLOR)
+
+    }
+    drawRect(rket.x, rket.y, RACKET_WIDTH, RACKET_HEIGHT, WHITE)
+    drawCorners(rket)
+    drawEdges(rket)
+
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+import pt.isel.canvas.*
+
+/**
+ * Program entry point for a version of MultiBall
+ */
+fun main() {
+    onStart{
+        val arena = Canvas(400, 600, BLACK)
+        val rck = Racket(RACKET_X, RACKET_Y)
+        arena.drawRacket(rck)
+    }
+    onFinish { }
+}
